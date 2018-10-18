d0ad071780d9a342a9244bfdec4db9f375b66ce7
Devollin <34558845+Devollin@users.noreply.github.com>
2018-10-17 22:32:21 -0500

    Create Config.lua

---
+++ testing/Config.lua
@@ -0,0 +1,33 @@
+--[[
+	T : Time
+	ES : EasingStyle
+	ED : EasingDirection
+	RC : RepeatCount
+	R : Repeat
+	DT : DelayTime
+	AP : AutoPlay
+	CB : Callback
+	BC : BezierCurves
+	SPS : StepsPerSecond
+	D : Delay
+--]]
+
+return {
+	TweenService = game:GetService'TweenService',
+	Easing = require(script.Parent.Easing),
+	
+	Defaults = {
+		T = 0.25,
+		ES = Enum.EasingStyle.Quart,
+		ED = Enum.EasingDirection.Out,
+		RC = 0,
+		R = false,
+		DT = 0,
+		AP = true,
+		CB = false,
+		BC = nil,
+		SPS = 60,
+		D = nil
+	}
+	
+}
