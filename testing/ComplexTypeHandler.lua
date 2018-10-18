6cbcdd932e98cc01d9b3ee8a8f4c0abebcc8c494
Devollin <34558845+Devollin@users.noreply.github.com>
2018-10-17 22:31:53 -0500

    Create ComplexTypeHandler.lua

---
+++ testing/ComplexTypeHandler.lua
@@ -0,0 +1,68 @@
+local Complex = {Primitives = {}}
+
+function Complex.Primitives.Bi(A, B) -- UDim, Vector2, Vector2int16
+	
+end
+
+function Complex.Primitives.Tri(A, B, C) -- Color3, Region3, Region3int16, 
+	
+end
+
+function Complex.Primitives.Poly(...)
+	
+end
+
+
+function Complex.CFrame()
+	
+end
+
+function Complex.Color3()
+	
+end
+
+function Complex.NumberRange()
+	
+end
+
+function Complex.Rect()
+	
+end
+
+function Complex.Region3()
+	
+end
+
+function Complex.Region3int16()
+	
+end
+
+function Complex.UDim()
+	
+end
+
+function Complex.UDim2()
+	
+end
+
+function Complex.Vector2int16()
+	
+end
+
+function Complex.Vector3()
+	
+end
+
+function Complex.Vector3int16()
+	
+end
+
+function Complex.ColorSequence()
+	
+end
+
+function Complex.NumberSequence()
+	
+end
+
+return Complex
