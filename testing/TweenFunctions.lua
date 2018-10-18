d5d9f856068ce949a1c91b69e061bfb555a26d81
Devollin <34558845+Devollin@users.noreply.github.com>
2018-10-17 22:35:49 -0500

    Create TweenFunctions.lua

---
+++ testing/TweenFunctions.lua
@@ -0,0 +1,19 @@
+local Functions = {Primitive = {}}
+
+function Functions.Play(Tweens)
+	
+end
+
+function Functions.Replay(Tweens)
+	
+end
+
+function Functions.Pause(Tweens)
+	
+end
+
+function Functions.Stop(Tweens)
+	
+end
+
+return Functions
