837430de85fc66fd91cb417544f133149d13c3af
Devollin <34558845+Devollin@users.noreply.github.com>
2018-10-20 13:51:03 -0500

    Update Bezier.lua

--- Libraries/Bezier.lua
+++ Libraries/Bezier.lua
@@ -1,51 +1,46 @@
-local Bezier = {}
+-- Created by EgoMoose
+-- URL : https://devforum.roblox.com/t/reparameterization-tips-and-tricks/135935
+-- Adjusted for Omnitween
 
-local function Lerp(A, B, C)
-	return A + (B - A) * C
+local function FixedBezier(Time, A, B, C)
+    return (1 - Time)^2 * A + 2 * (1 - Time) * Time * B + Time^2 * C
 end
 
-function Bezier.BuildPath(Vectors, CP)
-	local Points = {}
-	for Index = 1, #Vectors - 1 do
-		Points[Index] = Lerp(Vectors[Index], Vectors[Index + 1], CP)
+local function Simpson(Function, VarA, VarB, Number)
+    if (Number % 2 ~= 0) then
+        return
+    end
+    local H = (VarB - VarA) / Number
+    local S = Function(VarA) + Function(VarB)
+    for Index = 1, Number, 2 do
+        S = S + 4 * Function(VarA + Index * H)
+    end
+    for Index = 2, Number-1, 2 do
+        S = S + 2 * Function(VarA + Index * H)
+    end
+    return S * (H / 3)
+end
+
+return function(Time, A, B, C, Number)
+    local function ArcLength(Time)
+        local VarA = A - 2*B + C
+        local VarB = 2 * (B - A)
+        local A = 4 * VarA:Dot(VarA)
+        local B = 4 * VarA:Dot(VarB)
+        local C = VarB:Dot(VarB)
+        return math.sqrt(A * Time * Time + B * Time + C)
+    end
+
+    local Number = Number or 16
+    local Length = Simpson(ArcLength, 0, 1, Number)
+	if not Length then
+		return
 	end
-	if #Points >= 3 then
-		return Bezier.BuildPath(Points, CP)
-	end
-	return Lerp(Points[1], Points[2], CP)
-end
+    local Distance = Time * Length
 
-function Bezier.Curve(Vectors, T, S)
-	local Data = {
-		Pause = false,
-		Duration = T,
-		Steps = S,
-		Step = 0, 
-		TimePos = 0
-	}
-	local Curve = {
-		
-		Play = function()
-			Data.Pause = false
-			repeat
-				Bezier.BuildPath(Vectors, S)
-				wait(T / S)
-			until Data.Pause or Data.TimePos == 1
-		end,
-		
-		Pause = function()
-			Data.Pause = true
-		end,
-		
-		Stop = function()
-			Data.Pause = true
-			Data.Step = 0
-			Data.TimePos = 0
-		end
-		
-	}
-	Curve.Data = Data
-	return Curve
-end
+    for Index = 1, 5 do
+        Time = Time - (Simpson(ArcLength, 0, Time, Number) - Distance) / ArcLength(Time)
+    end
 
-return Bezier
+    return FixedBezier(Time, A, B, C)
+end
