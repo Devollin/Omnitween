3876cb4cc13811c7ac40c82a2b45509790f5584a
Devollin <34558845+Devollin@users.noreply.github.com>
2018-10-20 13:48:30 -0500

    Delete NativeTypeHandler.lua

--- Libraries/NativeTypeHandler.lua
+++
@@ -1,5 +0,0 @@
-local Config = require(script.Parent.Config)
-
-return function(Object, Properties, Data)
-	return Config.TweenService:Create(Object, TweenInfo.new(Data.T, Data.ES, Data.ED, Data.RC, Data.R, Data.DT), Properties)
-end
